311bc734ab3eaca9f4167c8f651afe4ebd90fdc0
Larry Cessna <larrybcessna@gmail.com>
2020-11-15 22:56:52 -0800

    Updated seeds with burger data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burgers (name) VALUES ('Mushroom and Swiss');
+INSERT INTO burgers (name) VALUES ('Big Mac');
+INSERT INTO burgers (name, eaten) VALUES ('Baconator', false);
+INSERT INTO burgers (name, eaten) VALUES ('Jumbo Jack', false);
+INSERT INTO burgers (name, eaten) VALUES ('Quarter Pounder', false);
+
